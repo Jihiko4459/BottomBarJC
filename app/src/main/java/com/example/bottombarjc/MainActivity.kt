4974dd23985ec415b02cb69781e62205e732743a
Jihiko4450 <anna.demyanenko.4459@mail.ru>
2024-04-15 20:55:30 +0500

    BottomBar

---
+++ app/src/main/java/com/example/bottombarjc/MainActivity.kt
@@ -0,0 +1,135 @@
+package com.example.bottombarjc
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.bottombarjc.ui.theme.BottomBarJCTheme
+import com.example.bottombarjc.ui.theme.GreenJC
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BottomBarJCTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    //modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyBottomAppBar()
+                }
+            }
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")//для работы с компонентом Scaffold
+@Composable
+fun MyBottomAppBar(){
+    val navigationController= rememberNavController()
+    val context= LocalContext.current.applicationContext
+    val selected=remember{
+        mutableStateOf(Icons.Default.Home)
+    }//выбранный по умолчанию элемент BottomNavigationBar
+
+
+    Scaffold (//помогает нескольким компонентам работать вместе
+        bottomBar = {
+            BottomAppBar(
+                containerColor = GreenJC
+            ) {
+                IconButton(onClick = {
+                    selected.value=Icons.Default.Home
+                    navigationController.navigate(Screens.Home.screen){
+                        popUpTo(0)
+                    }
+                },
+                    modifier = Modifier.weight(1f)) {
+                    Icon(Icons.Default.Home, contentDescription = null,
+                        modifier = Modifier.size(26.dp), tint=if(selected.value==Icons.Default.Home) Color.White else Color.DarkGray)
+                }
+                IconButton(onClick = {
+                    selected.value=Icons.Default.Search
+                    navigationController.navigate(Screens.Search.screen){
+                        popUpTo(0)
+                    }
+                },
+                    modifier = Modifier.weight(1f)) {
+                    Icon(Icons.Default.Search, contentDescription = null,
+                        modifier = Modifier.size(26.dp), tint=if(selected.value==Icons.Default.Search) Color.White else Color.DarkGray)
+                }
+                Box(modifier = Modifier
+                    .weight(1f)
+                    .padding(16.dp),
+                    contentAlignment = Alignment.Center){
+                    FloatingActionButton(onClick = {Toast.makeText(context, "Open Bottom Sheet", Toast.LENGTH_SHORT).show()}) {
+                        Icon(Icons.Default.Add, contentDescription = null, tint = GreenJC)
+                    }
+                }
+                IconButton(onClick = {
+                    selected.value=Icons.Default.Notifications
+                    navigationController.navigate(Screens.Notification.screen){
+                        popUpTo(0)
+                    }
+                },
+                    modifier = Modifier.weight(1f)) {
+                    Icon(Icons.Default.Notifications, contentDescription = null,
+                        modifier = Modifier.size(26.dp), tint=if(selected.value==Icons.Default.Notifications) Color.White else Color.DarkGray)
+                }
+                IconButton(onClick = {
+                    selected.value=Icons.Default.Person
+                    navigationController.navigate(Screens.Profile.screen){
+                        popUpTo(0)
+                    }
+                },
+                    modifier = Modifier.weight(1f)) {
+                    Icon(Icons.Default.Person, contentDescription = null,
+                        modifier = Modifier.size(26.dp), tint=if(selected.value==Icons.Default.Person) Color.White else Color.DarkGray)
+                }
+            }
+        }
+    )
+    {paddingValues ->
+        NavHost(navController = navigationController,
+            startDestination = Screens.Home.screen,
+            modifier = Modifier.padding(paddingValues)){
+            composable(Screens.Home.screen){ Home()}
+            composable(Screens.Search.screen){ Search() }
+            composable(Screens.Notification.screen){ Notification() }
+            composable(Screens.Profile.screen){ Profile() }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/bottombarjc/Screens.kt
@@ -0,0 +1,10 @@
+package com.example.bottombarjc
+
+//sealed class (закрытый класс) - класс, который имеет предопределенные (конечные) набор подклассов
+sealed class Screens(val screen:String) {
+    data object Home:Screens("home")//каждый data object представляет определенный экран
+    // и идентифицируется соответсвующим строковым идентификатором
+    data object Search:Screens("search")
+    data object Notification:Screens("notification")
+    data object Profile:Screens("profile")
+}
